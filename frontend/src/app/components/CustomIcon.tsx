e796c9a86a38b549f3663c4cd19274330a6e07ce
Vadims Mamedovs <vadims.mamedovs@gmail.com>
2025-05-06 15:40:44 +0300

    Extended db to secondary school grades

--- frontend/src/app/components/CustomIcon.tsx
+++ frontend/src/app/components/CustomIcon.tsx
@@ -2,16 +2,21 @@ import React from "react";
 import Biology from "./Icons/Biology";
 import Chemistry from "./Icons/Chemistry";
 import ComputerScience from "./Icons/ComputerScience";
+import Culture from "./Icons/Culture";
 import Design from "./Icons/Design";
 import Engineering from "./Icons/Enginnering";
 import Essential from "./Icons/Essential";
 import FineArts from "./Icons/FineArts";
 import Geography from "./Icons/Geography";
+import Graphic from "./Icons/Graphic";
 import History from "./Icons/History";
+import HistoryAndSocial from "./Icons/HistoryAndSocial";
 import LiteratureIcon from "./Icons/Literature";
 import MathIcon from "./Icons/Math";
 import MusicIcon from "./Icons/Music";
 import PhysicsIcon from "./Icons/Physics";
+import Project from "./Icons/Project";
+import Public from "./Icons/Public";
 import Social from "./Icons/Social";
 import Sports from "./Icons/Sports";
 import Talk from "./Icons/Talk";
@@ -27,6 +32,8 @@ const CustomIcon: React.FC<CustomIconProps> = ({ icon, size = 200 }) => {
       return <Chemistry size={size} />;
     case "computer-science":
       return <ComputerScience size={size} />;
+    case "culture":
+      return <Culture size={size} />;
     case "design":
       return <Design size={size} />;
     case "engineering":
@@ -37,8 +44,12 @@ const CustomIcon: React.FC<CustomIconProps> = ({ icon, size = 200 }) => {
       return <FineArts size={size} />;
     case "geography":
       return <Geography size={size} />;
+    case "graphic":
+      return <Graphic size={size} />;
     case "history":
       return <History size={size} />;
+    case "history-and-social":
+      return <HistoryAndSocial size={size} />;
     case "literature":
       return <LiteratureIcon size={size} />;
     case "math":
@@ -47,6 +58,10 @@ const CustomIcon: React.FC<CustomIconProps> = ({ icon, size = 200 }) => {
       return <MusicIcon size={size} />;
     case "physics":
       return <PhysicsIcon size={size} />;
+    case "project":
+      return <Project size={size} />;
+    case "public":
+      return <Public size={size} />;
     case "social":
       return <Social size={size} />;
     case "sports":

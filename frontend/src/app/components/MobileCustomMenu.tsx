d2db253972e0a48c8177b9b8ccd3d92fb71964d6
Vadims Mamedovs <vadims.mamedovs@gmail.com>
2025-05-12 07:26:16 +0300

    Added mobile menu function

--- frontend/src/app/components/MobileCustomMenu.tsx
+++ frontend/src/app/components/MobileCustomMenu.tsx
@@ -7,20 +7,34 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import AddIcon from "@mui/icons-material/Add";
 import SchoolIcon from "@mui/icons-material/School";
-
 import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
 import Link from "next/link";
-import { Collapse, ListItemButton, ListItemIcon } from "@mui/material";
+import {
+  Collapse,
+  ListItemButton,
+  ListItemIcon,
+  Typography,
+} from "@mui/material";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 
 export default function MobileCustomMenu({
   gradeGroups,
 }: {
   gradeGroups: GradeGroup[];
 }) {
-  const [open, setOpen] = React.useState(false);
-  const handleClick = () => {
-    setOpen(!open);
+  const [showGroups, setShowGroups] = React.useState(false);
+  const [openGroups, setOpenGroups] = React.useState<Record<number, boolean>>(
+    {}
+  );
+
+  const toggleGroup = (groupId: number) => {
+    setOpenGroups((prev) => ({
+      ...prev,
+      [groupId]: !prev[groupId],
+    }));
   };
+
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       <ListItemButton component={Link} href="/">
@@ -31,6 +45,7 @@ export default function MobileCustomMenu({
         </ListItemAvatar>
         <ListItemText primary="KPD saraksts" secondary="KPD reģistrs" />
       </ListItemButton>
+
       <ListItemButton component={Link} href="/sasniedzamie-rezultati">
         <ListItemAvatar>
           <Avatar>
@@ -40,43 +55,67 @@ export default function MobileCustomMenu({
         <ListItemText primary="SR saraksts" secondary="SR reģistrs" />
       </ListItemButton>
 
-      <ListItemButton onClick={handleClick}>
+      {/* Main toggle for all grade groups */}
+      <ListItemButton onClick={() => setShowGroups(!showGroups)}>
         <ListItemAvatar>
           <Avatar>
             <AddIcon />
           </Avatar>
         </ListItemAvatar>
         <ListItemText primary="Pievienot KPD" secondary="KPD reģistrs" />
+        {showGroups ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        {gradeGroups.map((group) =>
-          group.grades.map((grade) => (
-            <List component="div" disablePadding key={grade.grade_id}>
-              <ListItemButton
-                component={Link}
-                href={`/klases/${grade.grade_id}`}
-                sx={{ pl: 4 }}
-              >
-                <ListItemIcon>
-                  <SchoolIcon />
-                </ListItemIcon>
-                <ListItemText primary={grade.name} secondary={group.name} />
-              </ListItemButton>
-            </List>
-          ))
-        )}
+
+      {/* Collapse for all groups */}
+      <Collapse in={showGroups} timeout="auto" unmountOnExit>
+        {gradeGroups.map((group) => (
+          <React.Fragment key={group.grade_group_id}>
+            {/* Group-level toggle */}
+            <ListItemButton
+              onClick={() => toggleGroup(group.grade_group_id)}
+              sx={{ pl: 2 }}
+            >
+              <ListItemText primary={group.name} />
+              {openGroups[group.grade_group_id] ? (
+                <ExpandLess />
+              ) : (
+                <ExpandMore />
+              )}
+            </ListItemButton>
+
+            <Collapse
+              in={openGroups[group.grade_group_id]}
+              timeout="auto"
+              unmountOnExit
+            >
+              <List component="div" disablePadding>
+                {group.grades.map((grade) => (
+                  <ListItemButton
+                    key={grade.grade_id}
+                    component={Link}
+                    href={`/klases/${grade.grade_id}`}
+                    sx={{ pl: 4 }}
+                  >
+                    <ListItemIcon>
+                      <SchoolIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={grade.name} />
+                  </ListItemButton>
+                ))}
+              </List>
+            </Collapse>
+          </React.Fragment>
+        ))}
       </Collapse>
 
-      <Link key={3} href="/sasniedzamie-rezultati">
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Pievienot SR" secondary="SR reģistrs" />
-        </ListItem>
-      </Link>
+      <ListItemButton component={Link} href="/sasniedzamie-rezultati">
+        <ListItemAvatar>
+          <Avatar>
+            <AddIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Pievienot SR" secondary="SR reģistrs" />
+      </ListItemButton>
     </List>
   );
 }

0f2d1ecba967439ed06d1fdf7f2da19f51e8d7e7
Vadims Mamedovs <vadims.mamedovs@gmail.com>
2025-05-23 19:27:51 +0300

    Closed kpd register

--- frontend/src/app/components/CloseRegistration.tsx
+++ frontend/src/app/components/CloseRegistration.tsx
@@ -1,8 +1,33 @@
+import { Box, Stack, Typography } from "@mui/material";
+
 export default function CloseRegistration() {
   return (
-    <div className="closed-container">
-      <h1 style={{ color: "#e74c3c" }}>Slēgts</h1>
-      <p style={{ color: "#333" }}>Pieteikumu pieņemšana ir pabeigta.</p>
-    </div>
+    <Stack
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <Box
+        style={{
+          textAlign: "center",
+          margin: "100px auto",
+          maxWidth: "400px",
+          backgroundColor: "#fff",
+          padding: "20px",
+          borderRadius: "8px",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <Typography variant="h6" sx={{ color: "#e74c3c", fontWeight: "700" }}>
+          Slēgts
+        </Typography>
+        <Typography sx={{ color: "#333" }}>
+          Pieteikumu pieņemšana ir pabeigta.
+        </Typography>
+      </Box>
+    </Stack>
   );
 }

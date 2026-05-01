0cae9efce4bcef7505feffec07e2e1ee8b01cc96
dev-gopure <dev.gopure@gmail.com>
2026-05-01 06:54:24 +0300

    Initial commit to underwater

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,22 +1,7 @@
-// next.config.ts
-import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.resolve = {
-      ...(config.resolve || {}),
-      alias: {
-        ...(config.resolve?.alias || {}),
-        "@": path.resolve(__dirname, "src"),
-      },
-    };
-
-    return config;
-  },
-  allowedDevOrigins: [
-    "http://192.168.8.183:3000", // 👈 Add your IP here
-  ],
+  /* config options here */
 };
 
 export default nextConfig;

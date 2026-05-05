c08733d580fe740057ffd2c9f30b26d16a0cee00
dev-gopure <dev.gopure@gmail.com>
2026-05-05 21:04:45 +0300

    Fixed UI

--- app/grades/[gradeId]/page.tsx
+++ app/grades/[gradeId]/page.tsx
@@ -34,7 +34,7 @@ export default function CoursesPage() {
     <div className="flex flex-col gap-6 h-full px-2">
       <GradeHeader grade={grade} />
 
-      <div className="overflow-y-auto flex-1 custom-scroll">
+      <div className="overflow-y-auto flex-1 custom-scroll px-1">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 pt-1 pb-4">
           {courses?.map((c) => (
             <CourseCard
